60756b88c188b9b645491504ccb3ba10a5703e1e
jhonnierandrey <jhonnierandrey@outlook.com>
2020-05-05 19:53:55 -0300

    Se lee el .json directamente y se muestran sus datos en el index.ejs

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+const productsFilePath = path.join(__dirname, '../public/data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 const index= function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express', 'productos': products });
   };
 
 module.exports = index ;
